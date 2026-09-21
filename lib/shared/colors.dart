610f8413f2426cd8f07d247d0fd2cf1784312fde
Baset ali <ba3689468@gmail.com>
2026-09-21 16:23:34 +0200

    Create colors.dart

---
+++ lib/shared/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimary = Color(0xFF1A237E);
+const Color kAccent = Color(0xFFF48FB1);
+const Color kAccentDark = Color(0xFFC2185B);
+const Color kBackground = Color(0xFFF5F7FA);
+const Color kSoftPink = Color(0xFFFCE4EC);
